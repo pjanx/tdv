55d0f53f7aa6298cde818cdb637d3d6f7b29ecf1
Přemysl Eric Janouch <p@janouch.name>
2021-10-07 17:21:49 +0200

    Abandon the idea of sts for slovnik-cizich-slov
    We'd need something like inlining the type sequence within the data,
    which doesn't seem particularly elegant.

--- dicts/slovnik-cizich-slov.sh
+++ dicts/slovnik-cizich-slov.sh
@@ -1,7 +1,6 @@
 #!/bin/sh -e
 # Slovník cizích slov, see https://slovnik-cizich-slov.abz.cz/web.php/o-slovniku
-# TODO: Skipping the optional pronunciation field, tabfile can't handle it yet,
-# but could be made to accept a lowercase sametypesequence
+# XXX: skipping the /optional/ pronunciation field, tabfile can't handle that
 curl -Lo- https://slovnik-cizich-slov.abz.cz/export.php | \
 iconv -f latin2 -t UTF-8 | perl -CSD -F\\\| -le '
 	print "$_\t" . $F[2] =~ s/\\/\\\\/gr =~ s/; /\\n/gr for split(", ", $F[0])
