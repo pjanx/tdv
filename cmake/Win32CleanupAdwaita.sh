27a9869a6a68a2898e08b7c977941b6781788ecb
Přemysl Eric Janouch <p@janouch.name>
2021-10-26 11:26:39 +0200

    Trim down the Windows build a bit
    Remove most unused Adwaita icons and cursors, it's about 30 mebibytes
    of raw bullshit that doesn't seem to be missing in any shape or form.
    
    Rather sadly, the script is too complex to be rewritten in CMake script.

---
+++ cmake/Win32CleanupAdwaita.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -e
+# Removes unused icons from the Adwaita theme, it could be even more aggressive,
+# since it keeps around lots of sizes and all the GTK+ stock icons
+export LC_ALL=C
+find share/icons/Adwaita -type f | awk 'BEGIN {
+	while (("grep -aho \"[a-z][a-z-]*\" *.dll *.exe" | getline) > 0)
+		good[$0] = 1
+} /[.](png|svg|cur|ani)$/ {
+	# Cut out the basename without extensions
+	match($0, /[^\/]+$/)
+	base = substr($0, RSTART)
+	sub(/[.].+$/, "", base)
+
+	# Try matching while cutting off suffixes
+	while (!(keep = good[base]) &&
+		sub(/-(ltr|rtl|symbolic)$/, "", base)) {}
+	if (!keep)
+		print
+}' | xargs rm
